f2194bc01776fa6df9c5c97f8b7bb6a8496d03c9
ThatMovieGuy <131075466+ThatMovieGuyOriginal@users.noreply.github.com>
2025-03-19 15:15:46 -0600

    Update prisma.ts

--- src/lib/database/prisma.ts
+++ src/lib/database/prisma.ts
@@ -2,6 +2,12 @@
 import { PrismaClient } from '@prisma/client';
 import logger from '../utils/logger';
 
+// Augment the global object to include a prisma property
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
 // PrismaClient is attached to the `global` object in development to prevent
 // exhausting your database connection limit.
 let prisma: PrismaClient;
