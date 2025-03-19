a270d30e175394ff230d80f66c1af635ff2d5041
ThatMovieGuy <131075466+ThatMovieGuyOriginal@users.noreply.github.com>
2025-03-19 10:41:46 -0600

    Update CTAButton.tsx

--- src/components/marketing/CTAButton.tsx
+++ src/components/marketing/CTAButton.tsx
@@ -57,15 +57,24 @@ export const CTAButton: React.FC<CTAButtonProps> = ({
 
   return (
     <Link href={href}>
-      
+      <a
         onClick={handleClick}
         className={`font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 rounded-md transition-all duration-200 transform hover:scale-105 ${getButtonStyles()} ${getSizeStyles()} ${className}`}
         data-tracking-id={trackingId}
       >
         {text}
         {withArrow && (
-          <svg className="ml-2 -mr-1 w-4 h-4 inline" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path fillRule="evenodd" d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path>
+          <svg
+            className="ml-2 -mr-1 w-4 h-4 inline"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            />
           </svg>
         )}
       </a>
