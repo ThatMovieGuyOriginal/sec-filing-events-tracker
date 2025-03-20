81be4e858b0b96135dfe0334c9e9b87f7c4bab57
ThatMovieGuy <131075466+ThatMovieGuyOriginal@users.noreply.github.com>
2025-03-20 09:20:01 -0600

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,18 @@
-
+// jest.config.js
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    // Handle CSS imports
+    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    // Handle image imports
+    '^.+\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+};
