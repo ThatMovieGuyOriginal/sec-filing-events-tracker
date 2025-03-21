24a74936e6e243e607b16b92aabd806daa30aa50
ThatMovieGuy <131075466+ThatMovieGuyOriginal@users.noreply.github.com>
2025-03-21 14:59:27 -0600

    Update create-payment-intent.ts

--- src/pages/api/create-payment-intent.ts
+++ src/pages/api/create-payment-intent.ts
@@ -6,7 +6,7 @@ import { prisma } from '../../lib/database';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16', // Use the latest API version
+  apiVersion: '2023-08-16', // Use the compatible API version
 });
 
 // Get actual plan prices
